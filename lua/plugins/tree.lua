0139fa176dfe509e413f887f4ff6af8b8d3c38d2
Rob Simpson <rsimpson@contractor.usgs.gov>
2022-08-22 10:11:00 -0400

    Update nvim tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -15,67 +15,67 @@ local git_icons = {
 }
 
 local keymappings = {
-  { key = {"<CR>", "o", "<2-LeftMouse>"}, action = "edit" },
+  { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
   -- <C-e> keymapping cannot be set because it's used for toggling nvim-tree
   -- { key = "<C-e>",                        action = "edit_in_place" },
-  { key = {"O"},                          action = "edit_no_picker" },
-  { key = {"<2-RightMouse>", "<C-]>"},    action = "cd" },
-  { key = "<C-v>",                        action = "vsplit" },
-  { key = "<C-x>",                        action = "split" },
-  { key = "<C-t>",                        action = "tabnew" },
-  { key = "<",                            action = "prev_sibling" },
-  { key = ">",                            action = "next_sibling" },
-  { key = "P",                            action = "parent_node" },
-  { key = "<BS>",                         action = "close_node" },
-  { key = "<Tab>",                        action = "preview" },
-  { key = "K",                            action = "first_sibling" },
-  { key = "J",                            action = "last_sibling" },
-  { key = "I",                            action = "toggle_ignored" },
-  { key = "H",                            action = "toggle_dotfiles" },
-  { key = "R",                            action = "refresh" },
-  { key = "a",                            action = "create" },
-  { key = "d",                            action = "remove" },
-  { key = "D",                            action = "trash" },
-  { key = "r",                            action = "rename" },
-  { key = "<C-r>",                        action = "full_rename" },
-  { key = "x",                            action = "cut" },
-  { key = "c",                            action = "copy" },
-  { key = "p",                            action = "paste" },
-  { key = "y",                            action = "copy_name" },
-  { key = "Y",                            action = "copy_path" },
-  { key = "gy",                           action = "copy_absolute_path" },
-  { key = "[c",                           action = "prev_git_item" },
-  { key = "]c",                           action = "next_git_item" },
-  { key = "-",                            action = "dir_up" },
-  { key = "s",                            action = "system_open" },
-  { key = "q",                            action = "close" },
-  { key = "g?",                           action = "toggle_help" },
-  { key = "W",                            action = "collapse_all" },
-  { key = "S",                            action = "search_node" }
+  { key = { "O" }, action = "edit_no_picker" },
+  { key = { "<2-RightMouse>", "<C-]>" }, action = "cd" },
+  { key = "<C-v>", action = "vsplit" },
+  { key = "<C-x>", action = "split" },
+  { key = "<C-t>", action = "tabnew" },
+  { key = "<", action = "prev_sibling" },
+  { key = ">", action = "next_sibling" },
+  { key = "P", action = "parent_node" },
+  { key = "<BS>", action = "close_node" },
+  { key = "<Tab>", action = "preview" },
+  { key = "K", action = "first_sibling" },
+  { key = "J", action = "last_sibling" },
+  { key = "I", action = "toggle_ignored" },
+  { key = "H", action = "toggle_dotfiles" },
+  { key = "R", action = "refresh" },
+  { key = "a", action = "create" },
+  { key = "d", action = "remove" },
+  { key = "D", action = "trash" },
+  { key = "r", action = "rename" },
+  { key = "<C-r>", action = "full_rename" },
+  { key = "x", action = "cut" },
+  { key = "c", action = "copy" },
+  { key = "p", action = "paste" },
+  { key = "y", action = "copy_name" },
+  { key = "Y", action = "copy_path" },
+  { key = "gy", action = "copy_absolute_path" },
+  { key = "[c", action = "prev_git_item" },
+  { key = "]c", action = "next_git_item" },
+  { key = "-", action = "dir_up" },
+  { key = "s", action = "system_open" },
+  { key = "q", action = "close" },
+  { key = "g?", action = "toggle_help" },
+  { key = "W", action = "collapse_all" },
+  { key = "S", action = "search_node" }
 }
 
-require'nvim-tree'.setup {
+require 'nvim-tree'.setup {
   -- disables netrw completely
-  disable_netrw       = false,
+  disable_netrw                      = false,
   -- hijack netrw window on startup
-  hijack_netrw        = true,
+  hijack_netrw                       = true,
   -- open the tree when running this setup function
-  open_on_setup       = false,
+  open_on_setup                      = false,
   -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
+  ignore_ft_on_setup                 = {},
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
+  open_on_tab                        = false,
   -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
+  hijack_cursor                      = false,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = true,
+  update_cwd                         = true,
   -- opens in place of the unnamed buffer if it's empty
   hijack_unnamed_buffer_when_opening = false,
   --false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
-  respect_buf_cwd = true,
+  respect_buf_cwd                    = true,
   -- show lsp diagnostics in the signcolumn
-  diagnostics         = {
-    enable = false,
+  diagnostics                        = {
+    enable = true,
     icons  = {
       hint    = "",
       info    = "",
@@ -83,11 +83,11 @@ require'nvim-tree'.setup {
       error   = "",
     }
   },
-  renderer = {
+  renderer                           = {
     add_trailing = false,
     group_empty = true,
     highlight_git = true,
-    highlight_opened_files = "none",
+    highlight_opened_files = "name",
     root_folder_modifier = ":~",
     indent_markers = {
       enable = false,
@@ -98,13 +98,14 @@ require'nvim-tree'.setup {
       },
     },
     icons = {
+      git_placement = "after",
       glyphs = {
         git = git_icons
       }
     }
   },
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
+  update_focused_file                = {
     -- enables the feature
     enable      = true,
     -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
@@ -115,22 +116,22 @@ require'nvim-tree'.setup {
     ignore_list = {}
   },
   -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
+  system_open                        = {
     -- the command to run this, leaving nil should work in most cases
     cmd  = "",
     -- the command arguments as a list
     args = {}
   },
-  filters = {
+  filters                            = {
     dotfiles = false,
     custom = {}
   },
-  git = {
+  git                                = {
     enable = true,
     ignore = true,
     timeout = 500,
   },
-  actions = {
+  actions                            = {
     use_system_clipboard = true,
     change_dir = {
       enable = true,
@@ -151,7 +152,7 @@ require'nvim-tree'.setup {
       },
     },
   },
-  view = {
+  view                               = {
     -- width of the window, can be either a number (columns) or a string in `%`
     width = TREE_WIDTH,
     hide_root_folder = false,
@@ -167,7 +168,7 @@ require'nvim-tree'.setup {
     number = false,
     relativenumber = false
   },
-  trash = {
+  trash                              = {
     cmd = "trash",
     require_confirm = true
   }
@@ -175,10 +176,10 @@ require'nvim-tree'.setup {
 
 vim.api.nvim_set_keymap("n", "<C-e>", "<cmd>lua require'nvim-tree'.toggle()<CR>", { noremap = true, silent = true })
 
-nvim_tree_events.on_tree_open(function ()
-    bufferline_state.set_offset(TREE_WIDTH + 1, utils.add_whitespaces(13) .. 'File Explorer')
+nvim_tree_events.on_tree_open(function()
+  bufferline_state.set_offset(TREE_WIDTH + 1, utils.add_whitespaces(13) .. 'File Explorer')
 end)
 
-nvim_tree_events.on_tree_close(function ()
-    bufferline_state.set_offset(0)
+nvim_tree_events.on_tree_close(function()
+  bufferline_state.set_offset(0)
 end)
