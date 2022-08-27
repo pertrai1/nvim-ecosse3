8d3718542c59abead1a05ee968d78db25826f767
Rob Simpson <rsimpson2@me.com>
2022-08-26 22:21:08 -0400

    update nvim tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -90,7 +90,7 @@ require 'nvim-tree'.setup {
     highlight_opened_files = "name",
     root_folder_modifier = ":~",
     indent_markers = {
-      enable = true,
+      enable = false,
       icons = {
         corner = "└ ",
         edge = "│ ",
