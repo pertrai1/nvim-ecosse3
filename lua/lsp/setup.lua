910cafe211d81b4d65556d86f0ad9ffd01a09d43
Rob Simpson <rsimpson2@me.com>
2022-10-22 02:46:08 -0400

    Update LSP Servers & Capabilities

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -17,10 +17,11 @@ mason.setup {
 
 mason_lsp.setup {
   -- A list of servers to automatically install if they're not already installed
-  ensure_installed = { "bash-language-server", "css-lsp", "eslint-lsp", "graphql-language-service-cli", "html-lsp",
-    "json-lsp", "lua-language-server", "tailwindcss-language-server", "typescript-language-server",
-    "vetur-vls", "vue-language-server", "chrome-debug-adapter", "node-debug2-adapter", "prisma-language-server" },
-
+  ensure_installed = { "volar", "bashls", "cssls", "eslint", "html", "jsonls", "sumneko_lua", "tsserver" },
+  -- ensure_installed = { "bash-language-server", "css-lsp", "eslint-lsp", "graphql-language-service-cli", "html-lsp",
+  --   "json-lsp", "lua-language-server", "tailwindcss-language-server", "typescript-language-server",
+  --   "vetur-vls", "vue-language-server", "chrome-debug-adapter", "node-debug2-adapter", "prisma-language-server" },
+  --
   -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
   -- This setting has no relation with the `ensure_installed` setting.
   -- Can either be:
@@ -28,7 +29,7 @@ mason_lsp.setup {
   --   - true: All servers set up via lspconfig are automatically installed.
   --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
   --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-  automatic_installation = true,
+  automatic_installation = false,
 }
 
 local lspconfig = require("lspconfig")
@@ -56,7 +57,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 if cmp_nvim_lsp_ok then
-  capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
 capabilities.textDocument.foldingRange = {
